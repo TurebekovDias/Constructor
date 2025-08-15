ce76200724fb459889b657f7262e0dd98d4cd841
Turebekov Dias <diasdiasovit017@gmail.com>
2025-08-15 17:03:24 +0500

    create home page

--- frontend/front/src/app/app.routes.ts
+++ frontend/front/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { MainComponent } from './main.component/main.component'
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        component: MainComponent
+    }
+];
