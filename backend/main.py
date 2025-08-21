0e429701a8aeb83610aa3dd5fd3f0c31f04f80c4
Turebekov Dias <diasdiasovit017@gmail.com>
2025-08-21 08:36:34 +0500

    add back for auth

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,18 @@
-from fastapi import FastAPI
-from openai import OpenAI
+from fastapi import FastAPI, HTTPException, Depends
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+from datatime import datetime, timedelta
+from jose import JWTError, jwt
+# from openai import OpenAI
+
+import auth, models, utils
+
+
 
 app = FastAPI()
 
 @app.get("/")
 async def checkhealth():
     return {"Check": "Well"}
+@app.get("/test")
+async def test():
+    return auth.test()
