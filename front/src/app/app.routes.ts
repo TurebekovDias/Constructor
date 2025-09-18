fdd4c028135be5a4f103a9e1947479dfedcda66f
Turebekov Dias <diasdiasovit017@gmail.com>
2025-09-18 17:51:02 +0500

    created folders

--- front/src/app/app.routes.ts
+++ front/src/app/app.routes.ts
@@ -1,10 +1,4 @@
 import { Routes } from '@angular/router';
-import { MainComponent } from './pages/main.component/main.component'
-import { WorkSpaceComponent } from './pages/work-space.component/work-space.component';
-
 export const routes: Routes = [
-    {
-        path: '',
-        component: MainComponent,
-    }
+   
 ];
--- front/src/app/app.ts
+++ front/src/app/app.ts
@@ -1,13 +1,13 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet, RouterLink } from '@angular/router';
+import { Component } from '@angular/core';
+import { RouterOutlet,  } from '@angular/router';
 import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, MatButtonModule],
+  imports: [ MatButtonModule],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App {
-  protected readonly title = signal('front');
+  
 }
